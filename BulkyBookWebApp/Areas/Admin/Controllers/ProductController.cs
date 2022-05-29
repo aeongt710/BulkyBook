eb24a6327b7147748d9c4597db9883f2df154053
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-05-29 21:50:32 +0500

    viewbag and viewdata implemented

---
+++ BulkyBookWebApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using BukbyBook.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            //IEnumerable<Product> products = _unitOfWork.IProductRepository.GetAll();
+            return View();
+        }
+
+        public IActionResult Upsert(int? Id)
+        {
+            Product product = new();
+            IEnumerable<SelectListItem> categoryList = _unitOfWork.ICategoryRepository.GetAll().Select(
+                a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                });
+            IEnumerable<SelectListItem> coverList = _unitOfWork.ICoverRepository.GetAll().Select(
+                a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString()
+                });
+
+
+            if (Id == 0 || Id == null)
+            {
+                //Create Logic
+                ViewBag.categoryList = categoryList;
+                ViewData["coverList"] = coverList;
+                return View(product);
+            }
+            else
+            {
+                //Edit Logic
+            }
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.IProductRepository.Update(product);
+                _unitOfWork.Save();
+                TempData["warning"] = "Cover Updated Successfully!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == 0 || Id == null)
+                return NotFound("Invalid Product ID ");
+            Product product = _unitOfWork.IProductRepository.GetFirstOrDefault(a => a.Id == Id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName(nameof(Delete))]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+            Product product = _unitOfWork.IProductRepository.GetFirstOrDefault(a => a.Id == Id);
+            if (product != null)
+            {
+                _unitOfWork.IProductRepository.Remove(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Deleted Successfully!";
+                return RedirectToAction(nameof(Index), "Cover");
+            }
+            return NotFound();
+        }
+    }
+}
